5ed6a7a8680a947157b7975042fbe2ee9b8e426e
Gunko Vadim <guvacode@gmail.com>
2018-05-25 00:40:33 +0500

    ver 1.0

---
+++ src/backup/munit.pas
@@ -0,0 +1,63 @@
+unit mUnit;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Process, LCLType, Codebot.Input.Hotkeys;
+
+type
+
+  { THideFrm }
+  THideFrm= class(TDataModule)
+    procedure DataModuleCreate(Sender: TObject);
+    procedure DataModuleDestroy(Sender: TObject);
+  private
+    LedState:Boolean;
+    LedProcess: TProcess;
+    procedure KeyNotify(Sender: TObject; Key: Word; Shift: TShiftState);
+  public
+
+  end;
+
+var
+  HideFrm: THideFrm;
+
+implementation
+
+{$R *.lfm}
+
+{ THideFrm }
+
+procedure THideFrm.DataModuleCreate(Sender: TObject);
+begin
+  HotkeyCapture.RegisterNotify(VK_SCROLL,[],@KeyNotify);
+end;
+
+procedure THideFrm.DataModuleDestroy(Sender: TObject);
+begin
+  HotkeyCapture.UnRegisterNotify(VK_SCROLL,[]);
+end;
+
+procedure THideFrm.KeyNotify(Sender: TObject; Key: Word; Shift: TShiftState);
+begin
+  LedProcess := TProcess.Create(nil);
+   LedProcess.Options:=[poWaitOnExit,poNoConsole];
+   LedProcess.Executable := 'xset';
+     If LedState then
+      begin
+       LedProcess.Parameters.Add('-led');
+       LedState:=false;
+      end else
+      begin
+      LedProcess.Parameters.Add('led');
+      LedState:=true;
+      end;
+    LedProcess.Parameters.Add('3');
+    LedProcess.Execute;
+    LedProcess.Free;
+end;
+
+end.
+
---
+++ src/ledkey.lpr
@@ -0,0 +1,23 @@
+program ledkey;
+
+{$mode objfpc}{$H+}
+
+uses
+  {$IFDEF UNIX}{$IFDEF UseCThreads}
+  cthreads,
+  {$ENDIF}{$ENDIF}
+  Interfaces,
+  Forms, mUnit, ExSystemUtils
+  { you can add units after this };
+
+{$R *.res}
+
+begin
+  LSGlobalAddAtom(cMyHookAtom);
+  LSRunOnce;
+  //RequireDerivedFormResource:=True;
+  Application.Initialize;
+  Application.CreateForm(THideFrm, HideFrm);
+  Application.Run;
+end.
+
